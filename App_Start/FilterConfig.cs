addaca722d92ae196279b375e373616940a40fc5
vishnuharii <96003813+vishnuharii@users.noreply.github.com>
2024-08-16 02:31:26 -0400

    Add files via upload

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace HTTP5125_Cumulative_Project_Part_3
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
